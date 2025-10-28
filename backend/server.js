a6c41310450fdcb05b5d7c654f4da7bb2ca070a6
Abiyath Rahman <abiyathrahman@mail.weber.edu>
2025-10-28 14:56:24 -0600

    added server.js file and initialized server

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+require('dotenv').config({ path: './config.env'});
+const session = require('express-session');
+const app = express();
+const port = process.env.PORT || 4000;
+const cors = require('cors');
+const MongoStore = require('connect-mongo');
+const dbo = require('./db/conn');
+app.use(cors(
+    {
+        origin: "http://localhost:3000",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials:true,
+        optionsSuccessStatus: 204,
+        allowedHeaders: ["Content-Type", "Authorization"],
+
+    }
+));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.ATLAS_URI
+    })
+}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(port, () => {
+    dbo.connectToServer(function(err){
+        if(err){
+            console.log(err);
+        }
+    })
+    console.log(`Server is running on ${port}`);
+})
